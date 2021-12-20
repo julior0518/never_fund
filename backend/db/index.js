74b178f9750cdd095d64c5a14b0d31199f951282
julio <julior0518@gmail.com>
2021-12-20 09:54:31 -0500

    refactoring data

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/numverFundDatabase';
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/neverFundDatabase';
 
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
