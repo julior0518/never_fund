3097d4211dad69a155c9e244713368a92d59ef74
julio <julior0518@gmail.com>
2021-12-21 09:13:08 -0500

    CRUD working backend

--- backend/seed/taco.js
+++ backend/seed/taco.js
@@ -1,5 +1,5 @@
 const db = require('../db');
-const Taco = require('../models/taco');
+const { Taco } = require('../models/taco');
 
 // Connect to the database
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
