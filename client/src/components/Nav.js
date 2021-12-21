8f3eaa94c8e031a3a778121d04129aaa0f87c370
julio <julior0518@gmail.com>
2021-12-21 16:06:57 -0500

    added paths on Nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,19 @@
 import LogInOut from './LogInOut';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <div className="Nav">
       <p>Nav</p>
+      <Link to="/" className="li">
+        Landing
+      </Link>
+      <Link to="/profile" className="li">
+        Profile
+      </Link>
+      <Link to="/taco" className="li">
+        Taco
+      </Link>
       <LogInOut />
     </div>
   );
