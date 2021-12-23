24593fc25e389f4d6ea503f9caf42190b91c606a
julio <julior0518@gmail.com>
2021-12-22 20:03:40 -0500

    user created through axios call done

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,20 +3,18 @@ const Schema = mongoose.Schema;
 
 const User = new Schema(
   {
-    wallet: { type: String, required: true },
+    wallet: { type: String, required: false },
     nameUser: { type: String, required: true },
-    nameFirst : { type: String, required: true },
-    nameLast: { type: String, required: true },
-    email : { type: String, required: true },
-    phoneNumber : { type: Number, required: true },
-    address : { type: String, required: true },
-    password : { type: String, required: true },
-    description: { type: String, required: true },
-    image: { type: String, required: true },
-    ticketsCreated: [{ type: Schema.Types.ObjectId, ref: 'tickets'}],
-    votesCreated: [{ type: Schema.Types.ObjectId, ref: 'tickets'}],
-
-
+    nameFirst: { type: String, required: false },
+    nameLast: { type: String, required: false },
+    email: { type: String, required: false },
+    phoneNumber: { type: Number, required: false },
+    address: { type: String, required: false },
+    password: { type: String, required: true },
+    description: { type: String, required: false },
+    image: { type: String, required: false },
+    ticketsCreated: [{ type: Schema.Types.ObjectId, ref: 'tickets' }],
+    votesCreated: [{ type: Schema.Types.ObjectId, ref: 'tickets' }]
   },
   { timestamps: true }
 );
--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,34 +1,51 @@
+import axios from "axios";
 import { useRef, useState } from "react";
-import LogInOut from "./LogInOut";
+import { BASE_URL } from "../Globals";
 
 function UserForm(props) {
   const form = useRef()
   const [formType, setFormType] = useState({
     type: "login"
   })
+  const [user, setUser] = useState ({
+    nameUser: "",
+    password: "",
+    passwordConfirm: ""
+  })
+  function handleChange (e){
+    console.log(user)
+    e.preventDefault();
+    setUser({...user, [e.target.id]: e.target.value})
+  }
 
   function login(){}
-  function signup(){}
+  function signup(){
+    axios.post(`${BASE_URL}/users`, user)
+    console.log(`created`)
+  }
+
   let displayForm
   if (formType.type === "login"){
-    displayForm = <div className="displayForm">
+    displayForm = 
+    <div className="displayForm">
       <h2>Log In</h2>
-      <input placeholder="username" id="username" type="text" className="displayFormInput"></input>
-      <input placeholder="password" id="password" type="text" className="displayFormInput"></input>
+      <input onChange={handleChange}  placeholder="username" id="nameUser" type="text" className="displayFormInput"></input>
+      <input onChange={handleChange} placeholder="password" id="password" type="text" className="displayFormInput"></input>
       <button onClick={login}>submit</button>
       <p className="toSignUp" onClick={()=>{setFormType({...formType, type:"signup"})}}>Don't have an account? Sign up</p>
     </div>
   } else if (formType.type === "signup"){
-    displayForm = <div className="displayForm">
-    <h2>Create an account</h2>
-    <input placeholder="username" id="username" type="text" className="displayFormInput"></input>
-    <input placeholder="password" id="password" type="text" className="displayFormInput"></input>
-    <input placeholder="passwordConfirm" id="passwordConfirm" type="text" className="displayFormInput"></input>
-    <button onClick={login}>submit</button>
-    <p className="toSignUp" onClick={()=>{setFormType({...formType, type:"login"})}}>Already have an account? Log in</p>
-  </div>
+    displayForm = 
+    <div className="displayForm">
+      <h2>Create an account</h2>
+      <input onChange={handleChange} placeholder="username" id="nameUser" type="text" className="displayFormInput"></input>
+      <input onChange={handleChange} placeholder="password" id="password" type="text" className="displayFormInput"></input>
+      <input onChange={handleChange} placeholder="passwordConfirm" id="passwordConfirm" type="text" className="displayFormInput"></input>
+      <button onClick={signup}>submit</button>
+      <p className="toSignUp" onClick={()=>{setFormType({...formType, type:"login"})}}>Already have an account? Log in</p>
+    </div>
   }
- 
+
 
   function closePop (e){
     if(form.current === e.target){
